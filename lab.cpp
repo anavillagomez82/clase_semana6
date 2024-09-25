0f93b0a09824f76aeb2d1cc09cd8b1a3f16e8926
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-09-25 08:22:36 -0500

    Add files via upload

---
+++ lab.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    void printArray(const int arr(); const int size){
+        cout<<"{";
+        for (int i=0;i<size,i=i+1){
+            cout<<arr[i];
+        cout<<"}"
+        }
+    }
+    void printArray_revez(const int arr(); const int size){
+        cout<<"{";
+        for (int i=size;i>=0,i=i-1){
+            cout<<arr[i];
+        cout<<"}"
+        }
+    }
+}
